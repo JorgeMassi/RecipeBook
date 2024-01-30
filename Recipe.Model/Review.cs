175046b7ae22d8c67e0c2cdd1c0ad66639018b03
Jorge Massi <104774521+JorgeMassi@users.noreply.github.com>
2024-01-30 19:52:37 +0000

    Model

---
+++ Recipe.Model/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.Model
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string? Comment { get; set; }
+        public User? RecipeWriter { get; set; }
+        public Rating? Rating { get; set; }
+        public Recipe? Recipe { get; set; }
+
+    }
+}
